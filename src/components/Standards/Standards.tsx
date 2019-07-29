b92a865b5a00f06ba55ebb5862a97d41acb3fee6
Aleksei Butov <bootoffav@gmail.com>
2019-07-29 10:36:34 +1000

    Add 11612 to standards tree step 1

--- src/components/Standards/Standards.tsx
+++ src/components/Standards/Standards.tsx
@@ -1,14 +1,72 @@
 import React from 'react';
-import { inherits } from 'util';
+import './Standards.css';
 
-const Standards: React.FunctionComponent<{
+
+type IEN11612Detail = {
+  A1?: 'pass' | 'fail';
+  A2?: 'pass' | 'fail';
+  B?: 'pass' | 'fail';
+  C?: 'pass' | 'fail';
+  D?: 'pass' | 'fail';
+  E?: 'pass' | 'fail';
+  F?: 'pass' | 'fail';
+}
+
+class EN11612Detail extends React.Component<{
+  updateParent: (state: IEN11612Detail) => void;
+}>  {
+  blocks = ['A1', 'A2', 'B', 'C', 'D', 'E', 'F'];
+  state: IEN11612Detail = {};
+  
+  onChange = ({ currentTarget }: any) => this.setState({
+    [currentTarget.name]: currentTarget.dataset['result']
+  }, () => this.props.updateParent(this.state));
+
+  render() {
+    return <div className="d-flex">
+      {this.blocks.map(name => {
+        return <div key={name} className="flex-fill d-flex flex-column">
+          <p className="text-center">
+            <span className="m-1">{name}</span>
+          </p>
+          <div className="d-flex justify-content-center">
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="radio"
+                name={name}
+                data-result='fail'
+                onChange={this.onChange}
+              />
+              <label className="form-check-label"><span className="oi oi-circle-x"></span></label>
+            </div>&nbsp;&nbsp;&nbsp;
+          <div className="form-check">
+              <input
+                className="form-check-input"
+                type="radio"
+                name={name}
+                data-result='pass'
+                onChange={this.onChange}
+              />
+              <label className="form-check-label"><span className="oi oi-thumb-up"></span></label>
+            </div>
+          </div>
+        </div>
+      })}
+    </div>
+  }
+}
+
+type StandardsProps = {
+  updateParent: (state: EN11612Detail) => void;
   standards: string;
   standardsResult: {
     [key: string]: string;
   }
   resultChange: (el: React.SyntheticEvent) => void;
-}> = (props) => {
+};
 
+function Standards(props: StandardsProps) {
   if (props.standards === '') {
     return <div className="d-flex justify-content-center align-items-center h-100">
       <h5 className="text-uppercase">No standard chosen</h5>
@@ -23,96 +81,59 @@ const Standards: React.FunctionComponent<{
      * @param i used as key for React
    * @returns {JSX}
    */
-  const renderStandard = (standard: string, i: any) => {
+  function renderStandard(standard: string, i: any) {
     const id = standard.replace(/\s/g, '');
     return <div key={i}>
       <div className="card">
         <div className="card-header" id={`heading_${id}`}>
           <div className="d-flex">
-          <button className="btn btn-link" onClick={(e) => e.preventDefault()} data-toggle="collapse" data-target={`#collapse_${id}`} aria-expanded="true" aria-controls={`collapse_${id}`}>
-            {standard}
-          </button>
-          <div className="ml-auto form-check form-check-inline">
-            <input
-              className="form-check-input"
-              type="radio"
-              checked={props.standardsResult[standard] === 'fail'}
-              onChange={props.resultChange}
-              name={`radioOptions${id}`}
-              data-standard={standard}
-              id={`radioFail${id}`}
-              value="fail"
+            <button className="btn btn-link" onClick={(e) => e.preventDefault()} data-toggle="collapse" data-target={`#collapse_${id}`} aria-expanded="true" aria-controls={`collapse_${id}`}>
+              {standard}
+            </button>
+            <div className="ml-auto form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                checked={props.standardsResult[standard] === 'fail'}
+                onChange={props.resultChange}
+                name={`radioOptions${id}`}
+                data-standard={standard}
+                id={`radioFail${id}`}
+                value="fail"
+                />
+              <label className="form-check-label"><span className="oi oi-circle-x"></span></label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                checked={props.standardsResult[standard] === 'pass'}
+                onChange={props.resultChange}
+                name={`radioOptions${id}`}
+                data-standard={standard}
+                id={`radioPass${id}`}
+                value="pass"
               />
-            <label className="form-check-label"><span className="oi oi-circle-x"></span></label>
-          </div>
-          <div className="form-check form-check-inline">
-            <input
-              className="form-check-input"
-              type="radio"
-              checked={props.standardsResult[standard] === 'pass'}
-              onChange={props.resultChange}
-              name={`radioOptions${id}`}
-              data-standard={standard}
-              id={`radioPass${id}`}
-              value="pass"
-            />
-            <label className="form-check-label"><span className="oi oi-thumb-up"></span></label>
-          </div>
+              <label className="form-check-label"><span className="oi oi-thumb-up"></span></label>
+            </div>
           </div>
         </div>
 
         <div id={`collapse_${id}`} className="collapse show" aria-labelledby={`heading_${id}`} data-parent="#accordionStandards">
           <div className="card-body">
-            Здесь будут тесты
+            {standard === 'EN 11612'}
+             {/* && <EN11612Detail
+              // @ts-ignore
+              updateParent={props.updateParent} />} */}
           </div>
         </div>
       </div>
     </div>;
   }
 
-  return <>
-    {/* <span className="d-flex flex-row-reverse">
-      <button className="btn btn-sm btn-light toggle-accordion active mb-2" accordion-id="#accordion"></button>
-    </span> */}
-    <div className="accordion" id="accordionStandards">{standards.map(renderStandard)}</div>
-    </>;
+  return <div className="accordion" id="accordionStandards">{standards.map(renderStandard)}</div>;
 }
 
-const Tests = {
-  'EN 11611': [
-    'Flame spread - Face ignition(Fabrics /Seams/Accessories)',
-    'Flame spread - Button ignition(Fabrics /Seams/Accessories)',
-    'Tensile strength(woven fabrics)',
-    'Busting strength(knitted fabrics)',
-    'Tearing strength(for coated fabrics or laminates)',
-    'Impact spatter',
-    'Radiant heat',
-    'Electrical Resistance',
-    'pH',
-    'Dimensional stability - After 5 washes',
-    'Seam Strength',
-    'Industrial washes/Tumble dry - 50 cycles',
-  ],
-  'EN 11612': [
-    'Heat Resistance at 180º(Fabrics/Seams/Accessories)',
-    'Flame spread - A1 - Face ignition(Fabrics /Seams/Accessories)',
-    'Flame spread - A2 (optional test) - Bottom ignition(Fabrics /Seams/Accessories)',
-    'Dimensional stability - After 5 washes',
-    'Busting strength(knitted fabrics)',
-    'Tearing strength',
-  ],
-  'EN 1149-3': [],
-  'EN 13034': [
-    'Seams Tear Stregnht - All types of seams',
-    'Abrasion Resistance',
-    'Tear Resistance',
-    'Tensile Strength',
-    'Puncture Resistance',
-    'Penetration - Chemical Resistant4 chemicals products',
-    'Penetration - Chemical Resistant - GarmentTest',
-    'Report Handling Cost/ Test report Emission',
-  ]
-}
 
 
 export default Standards;
