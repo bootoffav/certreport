6abba85f2d8c6c665783a530d3f0063890e837bb
Aleksei Butov <bootoffav@gmail.com>
2019-10-30 16:59:15 +0800

    Add CompletedCertifications

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,39 +1,54 @@
 import React from 'react';
-import { Grid, Card } from 'tabler-react';
+import { Grid, Card, StatsCard } from 'tabler-react';
 import { Doughnut } from 'react-chartjs-2';
 import { getData } from './dataprocessing';
 import { doughnutConf } from './configs';
 import { DateFilter } from '../Filters';
+import "tabler-react/dist/Tabler.css";
 
-class Dashboard extends React.Component<{ tasks: any; }> {
+interface IDashboard {
+  tasks: any;
+  startDate?: Date;
+  endDate?: Date;
+}
+
+function tasksInRange(tasks: any[], filterParam: string, startDate?: Date, endDate?: Date) {
+  if (startDate === undefined || endDate === undefined) return tasks;
+
+  const tasksInRange = tasks.filter(task => {
+    const comparingDate = new Date(filterParam === 'certReceivedOnRealDate' ? task.state[filterParam] : task[filterParam]);
+    return startDate < comparingDate && endDate > comparingDate
+  });
+  debugger;
+  return tasksInRange;
+}
+
+
+class Dashboard extends React.Component<{ tasks: any; }, IDashboard> {
   state = {
-    tasks: this.props.tasks
+    tasks: this.props.tasks,
+    startDate: undefined,
+    endDate: undefined
   }
 
   dateFilter = (startDate: Date | null, endDate: Date | null): void => {
-    let tasksForUpdate: any;
     if (startDate === null || endDate === null) {
-      tasksForUpdate = this.state.tasks;
-    } else {
-      tasksForUpdate = this.state.tasks.filter((task: any) => {
-        const comparingDate = new Date(task.CREATED_DATE);
-        return startDate < comparingDate && endDate > comparingDate
-      });
+      return;
     }
 
     this.setState({
-      tasks: tasksForUpdate, startDate, endDate
+      tasks: tasksInRange(this.props.tasks, 'CREATED_DATE', startDate, endDate),
+      startDate, endDate
     });
   }
-  
+
   render() {
     return (
       <>
         <Grid.Row cards deck>
-          <Grid.Col width={4}>
+          <Grid.Col width={4} offset={4}>
             <Card body={ <DateFilter filter={this.dateFilter} /> }/>
           </Grid.Col>
-
         </Grid.Row>
         <Grid.Row cards deck>
           <Grid.Col md={6}>
@@ -47,9 +62,56 @@ class Dashboard extends React.Component<{ tasks: any; }> {
               }
             />
           </Grid.Col>
+          <Grid.Col md={6}>
+            <CompletedCertifications
+              tasks={this.props.tasks}
+              startDate={this.state.startDate}
+              endDate={this.state.endDate}
+            />
+            {/* <StatsCard layout={2} movement={5} total="423" label="Completed certifications" /> */}
+            {/* <StatsCard layout={2} movement={5} total="423" label="Completed certifications" /> */}
+            {/* <StatsCard layout={2} movement={5} total="423" label="Completed certifications" /> */}
+          </Grid.Col>
         </Grid.Row>
       </>
     );
+  }
+}
+
+class CompletedCertifications extends React.Component<{
+  tasks: any[];
+  startDate?: Date;
+  endDate?: Date;
+}> {
+
+  get completedCerts() {
+    const tasks = tasksInRange(
+      this.props.tasks,
+      'certReceivedOnRealDate',
+      this.props.startDate,
+      this.props.endDate
+    );
+
+    return tasks.length;
+  }
+  
+  get movement() {
+    const inRange = tasksInRange(
+      this.props.tasks,
+      'certReceivedOnRealDate',
+      this.props.startDate,
+      this.props.endDate
+    ).length;
+    const totalCerts = tasksInRange(
+      this.props.tasks,
+      'certReceivedOnRealDate',
+    ).length;
+
+    return (inRange * 100 / totalCerts).toFixed(2);
+  }
+
+  render() {
+    return <StatsCard layout={2} movement={this.movement} total={this.completedCerts} label="Completed certifications" />
   }
 }
 
