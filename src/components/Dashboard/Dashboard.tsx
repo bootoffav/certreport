58b43451a93d5b4fe0833b7dc973fbc8cecd292a
Aleksei Butov <bootoffav@gmail.com>
2019-10-30 13:30:04 +0800

    Add DateFilter to Dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,57 +1,54 @@
 import React from 'react';
-// import { AppContext } from '../../AppState';
 import { Grid, Card } from 'tabler-react';
 import { Doughnut } from 'react-chartjs-2';
 import { getData } from './dataprocessing';
 import { doughnutConf } from './configs';
+import { DateFilter } from '../Filters';
 
 class Dashboard extends React.Component<{ tasks: any; }> {
-  // static contextType = AppContext;
+  state = {
+    tasks: this.props.tasks
+  }
+
+  dateFilter = (startDate: Date | null, endDate: Date | null): void => {
+    let tasksForUpdate: any;
+    if (startDate === null || endDate === null) {
+      tasksForUpdate = this.state.tasks;
+    } else {
+      tasksForUpdate = this.state.tasks.filter((task: any) => {
+        const comparingDate = new Date(task.CREATED_DATE);
+        return startDate < comparingDate && endDate > comparingDate
+      });
+    }
+
+    this.setState({
+      tasks: tasksForUpdate, startDate, endDate
+    });
+  }
+  
   render() {
     return (
-      <div className="mt-1">
-        <div className="d-flex justify-content-center mb-1">
-          <Grid.Row cards deck>
-            <Grid.Col md={2}>
-              <Card body={1}/>
-            </Grid.Col>
-            <Grid.Col md={2}>
-              <Card body="2"/>
-            </Grid.Col>
-            <Grid.Col md={2}>
-              <Card body="3" />
-            </Grid.Col>
-            <Grid.Col md={2}>
-              <Card body="3" />
-            </Grid.Col>
-            <Grid.Col md={2}>
-              <Card body="3" />
-            </Grid.Col>
-          </Grid.Row>
-        </div>
-
+      <>
         <Grid.Row cards deck>
-          <Grid.Col md={4}>
+          <Grid.Col width={4}>
+            <Card body={ <DateFilter filter={this.dateFilter} /> }/>
+          </Grid.Col>
+
+        </Grid.Row>
+        <Grid.Row cards deck>
+          <Grid.Col md={6}>
             <Card
               title={<div className="text-center">Tasks by stages</div>}
               body={
                 <Doughnut
-                  data={() => getData(this.props.tasks, 'byStages')}
+                  data={() => getData(this.state.tasks, 'byStages')}
                   options={doughnutConf.options}
                 />
               }
             />
           </Grid.Col>
-          <Grid.Col md={4}>
-            <Card
-              body="2"
-            />
-          </Grid.Col>
-          <Grid.Col md={4}>
-            <Card body="3" />
-          </Grid.Col>
         </Grid.Row>
-      </div>
+      </>
     );
   }
 }
