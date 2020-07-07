df629c9409a8eb2c69b10fddc4fa3d5e5447379c
Aleksei Butov <bootoffav@gmail.com>
2020-07-07 11:53:12 +0800

    Add Testing Laboratory

--- src/defaults.ts
+++ src/defaults.ts
@@ -2,43 +2,44 @@ import B24 from './B24';
 import * as Sentry from '@sentry/browser';
 
 let selectOptions: {
-  brand: any[];
-  standards: any[];
-  testingCompany: any[];
-  articles: [];
-  stages: any[];
-  [key: string]: any;
-} = {
-  brand: [
-    { value: 'C_10033', label: 'XMT'},
-    { value: 'C_10035', label: 'XMF'},
-    { value: 'C_10037', label: 'XMS'},
-    { value: 'C_10041', label: 'XMG'}
-  ],
-  testingCompany: [
-    { value: 'Aitex (China)', label: 'Aitex (China)'},
-    { value: 'Aitex (Spain)', label: 'Aitex (Spain)'},
-    { value: 'BTTG (UK)', label: 'BTTG (UK)'},
-    { value: 'CENTROCOT (Italy)', label: 'CENTROCOT (Italy)'},
-    { value: 'CSI-Serico (Italy)', label: 'CSI-Serico (Italy)'},
-    { value: 'Satra (UK)', label: 'Satra (UK)'}
-  ],
-  stages: [
-    { value: '00. Paused', label: '00. Paused' },
-    { value: '0. Sample to be prepared', label: '0. Sample to be prepared' },
-    { value: '1. Sample Sent', label: '1. Sample Sent' },
-    { value: '2. Sample Arrived', label: '2. Sample Arrived' },
-    { value: '3. PI Issued', label: '3. PI Issued' },
-    { value: '4. Payment Done', label: '4. Payment Done' },
-    { value: '5. Testing is started', label: '5. Testing is started' },
-    { value: '6. Pre-treatment done', label: '6. Pre-treatment done' },
-    { value: '7. Test-report ready', label: '7. Test-report ready' },
-    { value: '8. Certificate ready', label: '8. Certificate ready' },
-    { value: '9. Ended', label: '9. Ended' }
-  ],
-  standards: JSON.parse(localStorage.getItem('standards') || '[]'),
-  articles: JSON.parse(localStorage.getItem('articles') || '[]')
-};
+    brand: any[];
+    standards: any[];
+    testingCompany: any[];
+    articles: [];
+    stages: any[];
+    [key: string]: any;
+    } = {
+    brand: [
+        { value: 'C_10033', label: 'XMT'},
+        { value: 'C_10035', label: 'XMF'},
+        { value: 'C_10037', label: 'XMS'},
+        { value: 'C_10041', label: 'XMG'}
+    ],
+    testingCompany: [
+        { value: 'Aitex (China)', label: 'Aitex (China)'},
+        { value: 'Aitex (Spain)', label: 'Aitex (Spain)'},
+        { value: 'BTTG (UK)', label: 'BTTG (UK)'},
+        { value: 'CENTROCOT (Italy)', label: 'CENTROCOT (Italy)'},
+        { value: 'CSI-Serico (Italy)', label: 'CSI-Serico (Italy)' },
+        { value: 'Leitat (Spain)', label: 'Leitat (Spain)' },
+        { value: 'Satra (UK)', label: 'Satra (UK)'}
+    ],
+    stages: [
+        { value: '00. Paused', label: '00. Paused' },
+        { value: '0. Sample to be prepared', label: '0. Sample to be prepared' },
+        { value: '1. Sample Sent', label: '1. Sample Sent' },
+        { value: '2. Sample Arrived', label: '2. Sample Arrived' },
+        { value: '3. PI Issued', label: '3. PI Issued' },
+        { value: '4. Payment Done', label: '4. Payment Done' },
+        { value: '5. Testing is started', label: '5. Testing is started' },
+        { value: '6. Pre-treatment done', label: '6. Pre-treatment done' },
+        { value: '7. Test-report ready', label: '7. Test-report ready' },
+        { value: '8. Certificate ready', label: '8. Certificate ready' },
+        { value: '9. Ended', label: '9. Ended' }
+    ],
+    standards: JSON.parse(localStorage.getItem('standards') || '[]'),
+    articles: JSON.parse(localStorage.getItem('articles') || '[]')
+    };
 
 export interface IState {
   standardsResult: {
