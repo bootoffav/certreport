04a45a16f5f4bd4fdf311f844c1a87ed50c408f9
Aleksei Butov <bootoffav@gmail.com>
2021-12-30 18:25:31 +0800

    Tabs ready for ExpiringCerts

--- src/components/ExpiringCerts/ExpiringCerts.tsx
+++ src/components/ExpiringCerts/ExpiringCerts.tsx
@@ -1,7 +1,60 @@
-function ExpiringCerts() {
+import type { TaskState } from 'Task/Task.interface';
+import './ExpiringCertsTab.css';
+
+type ExpiringCertsProps = {
+  tasks: TaskState[];
+};
+
+type TabProps = {
+  months: 1 | 3 | 6;
+};
+const Tab = ({ months }: TabProps) => {
   return (
-    <div className="px-4 py-5 my-5 text-center">
-      <h1 className="display-5 fw-bold">TO BE IMPLEMENTED SOON</h1>
+    <li className="nav-item" role="presentation">
+      <button
+        className={`nav-link ${
+          months === 6 ? 'active' : ''
+        } mx-auto expiringCertsTab`}
+        id={`months${months}-tab`}
+        data-bs-toggle="tab"
+        data-bs-target={`#months${months}`}
+        type="button"
+        role="tab"
+        aria-controls={`months${months}`}
+        aria-selected="true"
+      >
+        {months} month{months !== 1 ? 's' : ''}
+      </button>
+    </li>
+  );
+};
+
+const TabContent = ({ months }: TabProps) => {
+  return (
+    <div
+      className="tab-pane fade show active"
+      id={`months${months}`}
+      role="tabpanel"
+      aria-labelledby={`months${months}-tab`}
+    >
+      {months}
+    </div>
+  );
+};
+
+function ExpiringCerts({ tasks }: ExpiringCertsProps) {
+  return (
+    <div className="container">
+      <ul className="nav nav-tabs nav-justified" id="myTab" role="tablist">
+        {([6, 3, 1] as const).map((month) => (
+          <Tab months={month} />
+        ))}
+      </ul>
+      <div className="tab-content" id="myTabContent">
+        {([6, 3, 1] as const).map((month) => (
+          <TabContent months={month} />
+        ))}
+      </div>
     </div>
   );
 }
