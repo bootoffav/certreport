df442d1435db2be34e4faab2904b73ebd672937c
Aleksei Butov <bootoffav@gmail.com>
2022-07-27 11:54:45 +0300

    PaymentsThunk refactored

--- src/store/slices/PaymentsThunk.ts
+++ src/store/slices/PaymentsThunk.ts
@@ -5,30 +5,25 @@ import { Payment } from 'Task/Task.interface';
 
 const fetchPayments = createAsyncThunk(
   'common/fetchPayments',
-  async (): Promise<any> => {
-    let payments: {
-      [key: number]: Payment[];
-    } = {};
-    const { data: paymentSet } = (await DB.client().query(
-      q.Map(
-        q.Paginate(q.Documents(q.Collection('payments')), {
-          size: 100000, // max for faunadb for a single query
-        }),
-        q.Lambda('payment', q.Get(q.Var('payment')))
+  async (): Promise<Record<number, Payment[]>> =>
+    DB.client()
+      .query<{ data: any }>(
+        q.Map(
+          q.Paginate(q.Documents(q.Collection('payments')), {
+            size: 100000, // max for faunadb for a single query
+          }),
+          q.Lambda('payment', q.Get(q.Var('payment')))
+        )
       )
-    )) as any;
-
-    for (const {
-      data: { payments: paymentsPerTask },
-      ref: {
-        value: { id },
-      },
-    } of paymentSet) {
-      payments[id] = paymentsPerTask;
-    }
-
-    return payments;
-  }
+      .then(({ data }: any) =>
+        data.reduce(
+          (payments: Record<number, Payment[]>, { ref: { id }, data }: any) => {
+            payments[id] = data.payments;
+            return payments;
+          },
+          {}
+        )
+      )
 );
 
 export default fetchPayments;
