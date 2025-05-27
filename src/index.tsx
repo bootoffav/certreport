1d100284563b135817d6cd3b4f48cb89ef370f72
Aleksei Butov <bootoffav@gmail.com>
2025-05-27 13:43:57 +0300

    Add Auth0

--- src/index.tsx
+++ src/index.tsx
@@ -10,17 +10,36 @@ import store from './store/store';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App/App';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { useAuth0 } from '@auth0/auth0-react';
 
 initApp();
 
-run();
-
-function run() {
-  createRoot(document.getElementById('app')!).render(
-    <StrictMode>
+createRoot(document.getElementById('app')!).render(
+  <StrictMode>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN ?? ''}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID ?? ''}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
       <Provider store={store}>
-        <App />
+        <WrapWithAuth />
       </Provider>
-    </StrictMode>
-  );
+    </Auth0Provider>
+  </StrictMode>
+);
+
+function WrapWithAuth() {
+  const { loginWithRedirect, isAuthenticated, isLoading } = useAuth0();
+  if (isLoading) {
+    return (
+      <div className="centered is-flex is-justify-content-center">
+        HOLD ON A SECOND...
+      </div>
+    );
+  }
+
+  return isAuthenticated ? <App /> : <>{loginWithRedirect()}</>;
 }
